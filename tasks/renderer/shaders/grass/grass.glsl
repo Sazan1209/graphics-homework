85b7834dac4ac2764feb6ec07977806d0ead7510
Sazan1209 <Sazanovich.md@phystech.edu>
2025-06-08 20:06:13 +0300

    Beautify grass a bit

--- tasks/renderer/shaders/grass/grass.glsl
+++ tasks/renderer/shaders/grass/grass.glsl
@@ -2,13 +2,19 @@
 
 const vec2 terrainSize = vec2(1024, 1024);
 const vec3 centerCoordModel = vec3(terrainSize / 2.0, 0).xzy;
-const vec3 centerCoordWorld = vec3(0, -100, 0);
+const vec3 centerCoordWorld = vec3(0, -5.0, 0);
 
 // zInv(eyeW - centerW) + centerM = eyeM
-
+#define PI 3.1415926538
 
 // the size of the square around the player in which grass is rendered;
-const vec2 grassDrawSquare = vec2(32, 32);
+const vec2 grassDrawSquare = vec2(16, 16);
 const vec2 grassPerMeter = vec2(drawnGrassCount) / grassDrawSquare;
 
 const vec2 totalGrassCount = terrainSize * grassPerMeter;
+
+float rand(inout uint n)
+{
+  n *= 43758;
+  return fract(sin(n % 4096));
+}
